4a2c9b8b0134141310eece669a45648b23a84444
Wang Siyuan <v-siywang@microsoft.com>
2025-03-03 04:23:07 +0000

    add qwen72b deploy vllm script

---
+++ deploy_qwen.sh
@@ -0,0 +1,3 @@
+
+MODEL_PATH="/mnt/longcontext/models/siyuan/llama3/Qwen2.5-VL-72B-Instruct/"
+vllm serve ${MODEL_PATH} --api-key token-abc123 --tensor-parallel-size 4 --gpu-memory-utilization 0.9  --trust-remote-code --max_model_len 32768 --port 8000
